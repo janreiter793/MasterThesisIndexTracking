300ec3c76d73c2aea09806845b740be87a75750a
janreiter793 <70901834+janreiter793@users.noreply.github.com>
2025-04-07 22:01:40 +0200

    Add files via upload

--- refresh-time_sampler.R
+++ refresh-time_sampler.R
@@ -10,26 +10,61 @@
 #                                                                              #
 ################################################################################
 library(magrittr)
+library(dplyr)
+library(lubridate)
 
 ##### Functions #####
 # Takes the whole dataframe of stock prices, and turns time column into unit
 # time for each day. That is, for each day, the time points fall in the interval
-# [0, 1], where 0 is market open and 1 is market close.
+# [0, 1], where 0 is market open (09:30:00 EST) and 1 is market close (16:00:00 EST).
 msToUnitTime <- function(data) {
-  # Find each ticker symbol
-  symbols <- data$symb %>% unique
+  market_duration_ms <- 6 * 60 * 60 * 1000 + 30 * 60 * 1000
   
-  new_data <- data.frame()
-  for(symbol in symbols) { # Loop through each symbol
-    temp <- data %>% filter(symb == symbol) # Find subset of symbol
-    days <- temp$day %>% unique # Find all the unqiue days
-    for(d in days) { # For each day the time column is turned to unit time
-      temp_2 <- temp %>% filter(day == d)
-      temp_2$t <- temp_2$t - min(temp_2$t)
-      temp_2$t <- temp_2$t / max(temp_2$t)
-      new_data %<>% rbind(temp_2) # Append to new data set
-    }
+  data %>%
+    mutate(
+      # Convert milisecond time stamp to date and time
+      t_POSIXct = as.POSIXct(t / 1000, origin = "1970-01-01", tz = "EST"),
+      
+      # Find the date
+      date = as.Date(t_POSIXct, tz = "EST"),
+      
+      # Generate POSIXct objects specifying exact open and close times
+      market_open = as.POSIXct(paste0(date, " 09:30:00"), tz = "EST"),
+      market_close = as.POSIXct(paste0(date, " 16:00:00"), tz = "EST"),
+      
+      # Specify rows that are within trading hours
+      in_market_hours = t_POSIXct > market_open & t_POSIXct < market_close,
+      
+      # Calculate unit time by subtract market-open milisecond time from trade
+      # time in ms. Then divide by market open duration in ms
+      t_unit = (as.numeric(t_POSIXct) * 1000 - as.numeric(market_open) * 1000) / market_duration_ms
+    ) %>%
+    # Filter out trades that are not within trading hours
+    filter(in_market_hours) %>%
+    
+    # Remove unnesecary columns
+    select(-market_open, -market_close, -in_market_hours) %>% 
+    return
+}
+
+# Takes a list of vectors and returns the length of the shortest vector
+min_length <- function(l) {
+  l %>% 
+    lapply(FUN = length) %>% 
+    unlist %>% 
+    unname %>% 
+    min %>% 
+    return
+}
+
+# Takes a dataset of stock data with unit times, and then returns a new 
+# dataset, where observations are refresh-time sampled
+refreshTimes <- function(data) {
+  # Find the unique trading days
+  days <- data$day %>% unique
+  for(d in days) {
+    temp <- 
+      data %>% 
+      filter(day == d)
   }
-  
-  return(new_data)
 }
